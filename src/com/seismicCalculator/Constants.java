639f41d70caa8787117c2f3c303ba573c6f58d7c
benrendle <bm.rendle8@gmail.com>
2020-11-16 19:07:02 +0000

    Initial commit with first Java coding. Set up basic numax and dnu calculations with user input for mass, radius and teff.

---
+++ src/com/seismicCalculator/Constants.java
@@ -0,0 +1,17 @@
+package com.seismicCalculator;
+
+public enum Constants {
+    numax0 (3090.1),
+    dnu0 (135.1);
+//        final static double mass0 = 1.0;
+//        final static double rad0 = 1.0;
+//        final static double Teff0 = 4777.0;
+
+
+//    private final double constant;
+
+    Constants(double constant) {
+        this.constant;
+    }
+//    private double constant() {return constant;}
+}
